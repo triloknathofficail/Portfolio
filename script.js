5a4fdd65932f417f2c06fd708f395179d63f0e5e
triloknathofficail <114804518+triloknathofficail@users.noreply.github.com>
2023-02-10 18:06:35 +0530

    update

--- script.js
+++ script.js
@@ -60,11 +60,11 @@ document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
             document.title = "Portfolio | TRILOK NATH";
-            $("#favicon").attr("href", "assets/images/favicon.png");
+            $("#favicon").attr("href", "./photo/herroo.png");
         }
         else {
             document.title = "Come Back To Portfolio";
-            $("#favicon").attr("href", "assets/images/favhand.png");
+            $("#favicon").attr("href", "./photo/herroo.png");
         }
     });
 
